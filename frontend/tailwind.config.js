f24c6df9cc3ef162ae7dbd1be19a1ade4d4950b7
ONETheo <145070045+ONETheo@users.noreply.github.com>
2025-01-23 18:21:03 -0800

    added background and fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,11 +7,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#0AB7D4',
-        secondary: '#80F4E3',
-        background: '#111',
+        primary: {
+          DEFAULT: '#0AB7D4',
+          dark: '#098EA8'
+        },
+        secondary: {
+          DEFAULT: '#80F4E3',
+          dark: '#66C3B6'
+        },
+        background: {
+          DEFAULT: '#111111',
+          light: '#1A1A1A',
+          dark: '#0A0A0A'
+        }
       },
-    },
+      fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif']
+      }
+    }
   },
   plugins: [],
 }
