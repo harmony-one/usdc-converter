6d503f04ebd251b0091168bf66e275010d9b3483
ONETheo <145070045+ONETheo@users.noreply.github.com>
2025-01-23 18:18:48 -0800

    import updates useViewport.ts

--- frontend/src/hooks/useViewport.ts
+++ frontend/src/hooks/useViewport.ts
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { ViewState } from '@/types';
 
 export function useViewport(): ViewState {
   const [isMobileView, setIsMobileView] = useState<boolean>(false);
