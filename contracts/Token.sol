75af5387d49d3971fc90d0def79156bd99741b86
potvik <yuriy@harmony.one>
2025-02-18 18:27:30 +0300

    added support for tokens with different decimals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,12 +4,16 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract Token is ERC20Upgradeable, OwnableUpgradeable {
+    uint8 _decimals;
+
     function initialize(
         string memory name,
-        string memory symbol
+        string memory symbol,
+        uint8 decimals_
     ) public initializer {
         __ERC20_init(name, symbol);
         __Ownable_init(msg.sender);
+        _decimals = decimals_;
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
@@ -18,5 +22,9 @@ contract Token is ERC20Upgradeable, OwnableUpgradeable {
 
     function burn(address to, uint256 amount) public onlyOwner {
         _burn(to, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
     }
 }
--- contracts/TokenConverter.sol
+++ contracts/TokenConverter.sol
@@ -6,10 +6,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface MintableToken {
-    function mint(
-        address to,
-        uint256 amount
-    ) external;
+    function mint(address to, uint256 amount) external;
+}
+
+interface IERC20Decimals is IERC20 {
+    function decimals() external view returns (uint8);
 }
 
 contract TokenConverter is Ownable {
@@ -18,8 +19,9 @@ contract TokenConverter is Ownable {
     mapping(address => bool) public supportedTokens;
 
     MintableToken public outputToken;
+    uint8 public outputTokenDecimals;
 
-    event TokenConverted(address indexed user, address indexed inputToken, uint256 amount);
+    event TokenConverted(address indexed user, address indexed inputToken, uint256 inputAmount, uint256 outputAmount);
     event OutputTokenUpdated(address indexed previousToken, address indexed newToken);
     event SupportedTokenAdded(address indexed token);
     event SupportedTokenRemoved(address indexed token);
@@ -27,6 +29,7 @@ contract TokenConverter is Ownable {
     constructor(address[] memory inputTokens, address outputTokenAddress) Ownable(msg.sender) {
         require(outputTokenAddress != address(0), "Output token address cannot be zero");
         outputToken = MintableToken(outputTokenAddress);
+        outputTokenDecimals = IERC20Decimals(outputTokenAddress).decimals();
 
         for (uint256 i = 0; i < inputTokens.length; i++) {
             require(inputTokens[i] != address(0), "Input token address cannot be zero");
@@ -50,6 +53,7 @@ contract TokenConverter is Ownable {
         require(newOutputToken != address(0), "Output token address cannot be zero");
         address previousToken = address(outputToken);
         outputToken = MintableToken(newOutputToken);
+        outputTokenDecimals = IERC20Decimals(newOutputToken).decimals();
         emit OutputTokenUpdated(previousToken, newOutputToken);
     }
 
@@ -59,8 +63,21 @@ contract TokenConverter is Ownable {
 
         IERC20(inputToken).safeTransferFrom(msg.sender, address(this), amount);
 
-        outputToken.mint(msg.sender, amount);
+        uint8 inputDecimals = IERC20Decimals(inputToken).decimals();
+        uint256 normalizedAmount = _normalizeAmount(amount, inputDecimals, outputTokenDecimals);
 
-        emit TokenConverted(msg.sender, inputToken, amount);
+        outputToken.mint(msg.sender, normalizedAmount);
+
+        emit TokenConverted(msg.sender, inputToken, amount, normalizedAmount);
+    }
+
+    function _normalizeAmount(uint256 amount, uint8 inputDecimals, uint8 targetDecimals) internal pure returns (uint256) {
+        if (inputDecimals == targetDecimals) {
+            return amount;
+        } else if (inputDecimals > targetDecimals) {
+            return amount / 10 ** (inputDecimals - targetDecimals);
+        } else {
+            return amount * 10 ** (targetDecimals - inputDecimals);
+        }
     }
 }
